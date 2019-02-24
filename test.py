91c097fdb022ab505098740b064896ce0f673103
azkevin <karindaeng@gmail.com>
2019-02-23 19:11:44 -0500

    add beautiful soup stuff

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import io
 import os
 import requests
+from bs4 import BeautifulSoup
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -8,6 +9,7 @@ from google.cloud.vision import types
 
 # Import wikipedia library for getting the description
 import wikipedia
+import json
 
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
@@ -40,4 +42,11 @@ for label in labels:
 		print (wikipedia.summary(chosenLabel, sentences=3))
 
 		# Scrape price from Walmart
-		
+		URL = "https://www.walmart.ca/search/banana/N-117"
+		page = requests.get(URL,headers={"User-Agent":"Defined"})
+		json = page.json()
+
+		soup = BeautifulSoup(page.content, "html.parser")
+		price = json
+		#price = soup.find(class_="price-current width-adjusted").get_text()
+		print(price)
